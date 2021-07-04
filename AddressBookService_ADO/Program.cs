351fa4ce813c9c83127656742bcaadd4885210e9
pampapathi73 <pampapathik32@gmail.com>
2021-07-04 16:34:58 +0530

    [pampapathi] Added: AddressBook Service

---
+++ AddressBookService_ADO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookService_ADO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AddressBook Service");
+        }
+    }
+}
