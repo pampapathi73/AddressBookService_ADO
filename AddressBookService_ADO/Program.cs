d89919ac1275ac6dfafc0e876e4079b9fc613e10
pampapathi73 <pampapathik32@gmail.com>
2021-07-04 17:17:46 +0530

    [pampapathi] Added: AbilityToInsertNewContacts

--- AddressBookService_ADO/Program.cs
+++ AddressBookService_ADO/Program.cs
@@ -7,6 +7,23 @@ namespace AddressBookService_ADO
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to AddressBook Service");
+            PersonRepo repo = new PersonRepo();
+            PersonModel employee = new PersonModel();
+            employee.FirstName = "Mukti";
+            employee.LastName = "hk";
+            employee.Address = "House no 22";
+            employee.PhoneNumber = "6302907678";
+            employee.zip = 894523;
+            employee.City = "Tsk";
+            employee.State = "Tripura";
+            employee.Email = "muktihk@gmail.com";
+            employee.AddressBookName = "book4";
+            employee.Type = "Family";
+
+            if (repo.AddEmployee(employee))
+                Console.WriteLine("Records added successfully");
+            Console.ReadKey();
         }
     }
+    
 }
---
+++ AddressBookService_ADO/store_procedureSpAddPersonDetails.sql
@@ -0,0 +1,21 @@
+﻿create procedure [dbo].[SpAddPersonDetails]
+(
+@FirstName varchar(255),
+@LastName varchar(255),
+@Address varchar(255),
+@PhoneNumber varchar(255),
+@zip int,
+@City varchar(255),
+@State varchar(255),
+@Email varchar(255),
+@AddressBookName varchar(255),
+@Type varchar(255)
+)
+as
+begin
+insert into AddressBookDB values
+(
+@FirstName,@LastName,@Address,@City,@State,@zip,@PhoneNumber,@Email,@AddressBookName,@Type
+)
+end
+GO
